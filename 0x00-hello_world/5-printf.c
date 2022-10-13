a6cb6e5effbf79b7df02b14e52612e9078e54317
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-13 07:32:05 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  * main - Print a text
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	Printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
