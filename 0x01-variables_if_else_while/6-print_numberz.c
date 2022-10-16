b7df6e94fbbfff686d3a33a4cd75ab8b40f27ba4
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-16 02:22:15 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints type char 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
