f085327d632709a6bf07b615b44e3ba785ca53f3
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-16 02:04:39 -0700

    gitits from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - prints base of 10 with a single digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+		printf("\n");
+
+	return (0);
+
+}
