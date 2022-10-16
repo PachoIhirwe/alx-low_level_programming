7d22b536ad408026ee1575f0f2d4f904e4f51ddd
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-16 02:54:13 -0700

    last last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combination of single digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	putchar(num);
+	if (num != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}}
+	putchar('\n');
+	return (0);
+}
