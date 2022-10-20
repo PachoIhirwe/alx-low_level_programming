00027669d15445c10009c1d65f3d2c988ae746f8
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-20 08:10:48 -0700

    wahala

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper: A function that cheks uppercase
+ * _supper: function
+ * @c: an inputc char
+ * Return: 1or 0
+ */
+int _supper(int c)
+{
+	char uppercase = 'A';
+
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+		isupper = i;
+		break;
+	}
+	}
+}
