fd27c154604150a300f57af8c75afd797d967fef
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-10-21 03:53:54 -0700

    hot fix fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,17 @@ int main(void)
 	{
 		printf(" ");
 		if (n % 3 == 0)
+		{
 		printf("Fizz");
-		if (n & 5 == 0)
+		}
+		if (n % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		if (n % 3 != 0 && n % 5 != 0)
+		{
 			printf("%d", n);
+		}
 	}
 	printf("\n");
 			return (0);
