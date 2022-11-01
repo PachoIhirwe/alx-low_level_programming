d1fb101c0e0f26c989dd3f8f151328bebc1316cf
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-01 04:25:17 -0700

    ubukonje ni dange

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - memory copy
+ * @dest: destination
+ * @src: is sorurce
+ * @n: number of bytes
+ *
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
