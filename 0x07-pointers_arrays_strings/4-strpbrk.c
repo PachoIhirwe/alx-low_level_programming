4b74be0f59fa536b011ea8dd3c1f533d0c6ca1cc
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-01 04:03:28 -0700

    takeoff

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search for a string of set tupes
+ * @s: source string
+ * @accept: accept a char
+ *
+ * Return: the string since
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+			b++;
+		}
+		a++;
+	}
+}
