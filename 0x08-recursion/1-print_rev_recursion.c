70e1ddf63964bda1a94222d47fe4dd157e40bb72
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-02 05:04:49 -0700

    recursed 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string
+ * @s: str to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
