bd06a0b5156ff96e44c3153d8aeccfc648dddce1
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-02 06:54:20 -0700

    before confusion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - prints a string
+ * @n: str to be printed
+ *
+ * Return: The lenth str
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n >= 0 && n <= 1)
+	{
+		return (1);
+	}
+}
