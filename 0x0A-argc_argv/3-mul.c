a94ca405a2b0b1cbd324dc22ee3d3f6784c39f9b
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-06 23:19:43 -0800

    Monday started

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int result;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
-	printf("error \n");
+	printf("%s\n", "Error");
 	return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
-	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
