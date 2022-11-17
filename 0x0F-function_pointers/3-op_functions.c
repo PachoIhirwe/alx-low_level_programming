5a5c289bc6e4ecdce9034f0f569a14ebcef2d24a
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-17 02:09:35 -0800

    last func poin

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates sum
+ * @a: int
+ * @b: int
+ *
+ * Return: a value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calc difference btn a and b
+ * @a:int
+ * @b: int
+ *
+ * Return: a diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication a and b
+ * @a: int
+ * @b: int
+ * Return: multi
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - division of a and b
+ * @a: int
+ * @b: int
+ *
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos of a and b
+ * @a: int
+ * @b: int
+ * Return: mod a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
