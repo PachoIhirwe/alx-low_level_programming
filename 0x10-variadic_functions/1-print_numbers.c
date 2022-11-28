a495f9d84e221b0109d1774d951527ef6551b2eb
‘PachoIhirwe’ <alainpa18@gmail.com>
2022-11-28 02:54:54 -0800

    task 2 varidiac

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numb
+ * @separator: the string
+ * @n: The number fo integeres
+ * @...: var for numto print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index;
+
+	va_start(nums, n);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
